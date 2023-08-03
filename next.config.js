504e00cb8f05525115e3b15f70881da4978e5e24
jdcoto <jd506@hotmail.com>
2023-08-03 13:44:13 -0600

    feat(pokemon): added pokemon feature

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'lh3.googleusercontent.com'
-        },
-  
-      ]
-    }
-  
-  }
-  
-  module.exports = nextConfig
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
